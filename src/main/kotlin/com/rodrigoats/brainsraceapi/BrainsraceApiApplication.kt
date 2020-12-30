ae0093557a09921f58e46f253efe79391ccfca52
Rodrigo <rodrigoats1@gmail.com>
2020-12-30 20:58:01 +0000

    Creating project from start.spring.io

---
+++ src/main/kotlin/com/rodrigoats/brainsraceapi/BrainsraceApiApplication.kt
@@ -0,0 +1,11 @@
+package com.rodrigoats.brainsraceapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BrainsraceApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<BrainsraceApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/rodrigoats/brainsraceapi/BrainsraceApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rodrigoats.brainsraceapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BrainsraceApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
